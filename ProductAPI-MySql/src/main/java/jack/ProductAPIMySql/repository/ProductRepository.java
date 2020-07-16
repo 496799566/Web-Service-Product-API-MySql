6e4c80f40500cac553958bc45601273ed3f315f0
Yuanhao Chen <44731328+496799566@users.noreply.github.com>
2020-07-16 00:15:38 -0400

    3. Created The ProductRepository Inteface
    An subclass of the JpaRepository - contains queries that perform database operations

---
+++ ProductAPI-MySql/src/main/java/jack/ProductAPIMySql/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package jack.ProductAPIMySql.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jack.ProductAPIMySql.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
