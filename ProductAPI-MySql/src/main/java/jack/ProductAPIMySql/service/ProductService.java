9404d7a85765b5edcdbc15b45fb7846cf0890390
Yuanhao Chen <44731328+496799566@users.noreply.github.com>
2020-07-16 00:24:24 -0400

    5. Created ProductService
    The ProductSerivce class provides ready-to-go methods that are specifically designed for the ProductController

---
+++ ProductAPI-MySql/src/main/java/jack/ProductAPIMySql/service/ProductService.java
@@ -0,0 +1,35 @@
+package jack.ProductAPIMySql.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jack.ProductAPIMySql.model.Product;
+import jack.ProductAPIMySql.repository.ProductRepository;
+
+@Service
+@Transactional
+public class ProductService {
+ 
+    @Autowired
+    private ProductRepository repo;
+     
+    public List<Product> listAll() {
+        return repo.findAll();
+    }
+     
+    public void save(Product product) {
+        repo.save(product);
+    }
+     
+    public Product get(Integer id) {
+        return repo.findById(id).get();
+    }
+     
+    public void delete(Integer id) {
+        repo.deleteById(id);
+    }
+}
