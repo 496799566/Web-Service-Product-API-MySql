c37c3233731e60d1f0bc3fc6d7d8f0e6d075d0e8
Yuanhao Chen <44731328+496799566@users.noreply.github.com>
2020-07-15 23:51:44 -0400

    1. Created starter project with Maven in Eclipse
    A new Spring boot starter project with 3 dependencies: Spring web, Spring data JPA and the MySQL Driver

---
+++ ProductAPI-MySql/src/main/java/jack/ProductAPIMySql/ProductApiMySqlApplication.java
@@ -0,0 +1,13 @@
+package jack.ProductAPIMySql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiMySqlApplication.class, args);
+	}
+
+}
